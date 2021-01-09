f54e29d1572b02b05e9238f97f9d13f061b74a88
Sierra Wallace <sskwallace@gmail.com>
2021-01-09 23:02:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-      if @@items.include?(search_term)
+     if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
       else
