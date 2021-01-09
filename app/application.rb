8cfcd9d59eaf7eb4114b43b9293212d697c0b86a
Sierra Wallace <sskwallace@gmail.com>
2021-01-09 23:04:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,10 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
+   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+  end
    if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
