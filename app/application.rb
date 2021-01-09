d00a95b4d3a61091a66c27dd9b39299e0d9c070b
Sierra Wallace <sskwallace@gmail.com>
2021-01-09 23:04:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
    def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-  end
+  
    if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
